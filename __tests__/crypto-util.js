3a376fa9b6dbfd4c02a928764092a939b57e9fe2
Evgeny Vlasenko <mahnunchik@gmail.com>
2021-03-01 22:01:18 +0200

    feat: some function from crypto util

---
+++ __tests__/crypto-util.js
@@ -0,0 +1,36 @@
+import '@jest/globals';
+import fs from 'fs/promises';
+import cryptoUtil from '../lib/crypto-util.js';
+
+// https://github.com/monero-project/monero/blob/v0.17.1.9/tests/crypto/tests.txt
+const tests = (await fs.readFile('./__tests__/tests.txt', { encoding: 'utf8' })).split('\n');
+
+describe('crypto-util', () => {
+  for (const item of tests) {
+    const [cmd, ...rest] = item.split(' ');
+    switch (cmd) {
+      case 'check_scalar': {
+        const [scalar, expected] = rest;
+        test(`checkScalar '${scalar}' is valid '${expected}'`, () => {
+          expect(cryptoUtil.checkScalar(Buffer.from(scalar, 'hex'))).toBe(expected === 'true');
+        });
+        break;
+      }
+      case 'random_scalar': {
+        // no tests for random_scalar
+        break;
+      }
+      case 'hash_to_scalar': {
+        const [data, expected] = rest;
+        test(`hashToScalar '${data}' is scalar '${expected}'`, () => {
+          const actual = cryptoUtil.hashToScalar(Buffer.from(data, 'hex', 32));
+          expect(actual.equals(Buffer.from(expected, 'hex'))).toBe(true);
+        });
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+});
